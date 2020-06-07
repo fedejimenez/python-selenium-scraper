4c22c12979a93cfb19c03d6e8b198b40b5bb4292
Federico Jimenez <fedeejimenez@gmail.com>
2020-06-07 18:50:25 +0100

    Add basic scraper code

---
+++ scraper.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+import pdb
+# Debugger - colocar donde se desea: pdb.set_trace()
+
+# open firefox - headless mode
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
+
+# voy a la pagina 
+driver.get("https://ar.indeed.com/?r=us")
+print("Accediendo al navegador")
+
+# ingreso lo que busco
+titulo = driver.find_element_by_name("q")
+titulo.send_keys("Python")
+
+# ingreso lugar
+lugar = driver.find_element_by_name("l")
+lugar.send_keys("Argentina")
+
+# envio el formulario
+lugar.submit()
+print("Enviando formulario")
+
+# creo un archivo y lo abro
+archivo = open("trabajos.txt", "x")
+archivo = open("trabajos.txt", "a")
+
+# pausa para evitar error de carga
+timeout = 3 # segundos
+try:
+    element_present = EC.presence_of_element_located((By.ID, 'resultsCol'))
+    WebDriverWait(driver, timeout).until(element_present)
+    print("Comienza lectura de datos")
+except TimeoutException:
+    print("Error de carga")
+
+# obtengo todos los links
+links = driver.find_elements_by_css_selector('#resultsCol div a')
+
+# imprimo los datos
+print("Escribiendo datos en archivo")
+for elem in links:
+    href = elem.get_attribute('href')
+    if href is not None and "https://ar.indeed.com/rc" in href:
+        # print(href)
+        archivo.write(elem.get_attribute('text'))
+        archivo.write("\n")
+        archivo.write(href)
+        archivo.write("\n")
+        archivo.write(" ------------------------------------------ ")
+        archivo.write("\n")
+
+# cierro el browser y el archivo
+driver.close()
+archivo.close()
+print("Datos guardados")
